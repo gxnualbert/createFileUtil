37cf9f41563fd56d348afc47f244dfb16abd6956
gxnualbert <albert.chenwesoft@gmail.com>
2016-08-20 19:51:57 +0800

    update

--- src/createFileUtil.java
+++ src/createFileUtil.java
@@ -6,24 +6,26 @@ import java.io.IOException;
  */
 public class createFileUtil {
     public static void main(String[] args) {
-
-        createFile("secondFile.txt");
+        String dirName="c:/javaDir";
+        createDir(dirName);
+        String fileName="javatestfile.txt";
+        String  createFileName=dirName+"/"+fileName;
+        createFile(createFileName);
     }
 
     /*Create a file, doesn't specify the path*/
     public static boolean createFile(String createdFileName) {
         File file = new File(createdFileName);
         if (file.exists()) {
-            System.out.println("File: " + file.getName() + " already exist, no need to create. File Path: "+file.getAbsolutePath());
+            System.out.println("File: " + file.getName() + " already exist, no need to create. File Path: " + file.getAbsolutePath());
             return false;
         }
         try {
             if (file.createNewFile()) {
-                System.out.println("File: "+file.getName()+" create successfullly!! File path: "+file.getAbsolutePath());
+                System.out.println("File: " + file.getName() + " create successfullly!! File path: " + file.getAbsolutePath());
                 return true;
-            }
-            else{
-                System.out.println("Fail to create file "+file.getName());
+            } else {
+                System.out.println("Fail to create file " + file.getName());
                 return false;
             }
         } catch (IOException e) {
@@ -32,5 +34,20 @@ public class createFileUtil {
         }
     }
 
+    public static boolean createDir(String destDirName) {
+        File dir=new File(destDirName);
+        if (dir.exists()){
+            System.out.println("The directory "+dir.getName()+" already exist, no need to create");
+            return false;
+        }
+        if (dir.mkdir()){
+            System.out.println("create directory successfully, the path is "+dir.getAbsolutePath());
+            return true;
+        }
+        else{
+            System.out.println("fail to create directory");
+            return  false;
+        }
+    }
 
 }
